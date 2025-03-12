bccc3f0657448da6928c6a916599f346a8e5468b
johannesbraun54 <johannesbraun02@gmail.com>
2025-03-12 15:08:09 +0100

    all profile urls ready

--- coderr_app/api/urls.py
+++ coderr_app/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ImageUploadView, ProfileView
+from .views import ImageUploadView, ProfileView, ProfileBusinessListView, ProfileCustomerListView
 
 urlpatterns = [
     path('uploads/', ImageUploadView.as_view(), name='image-upload'),
-    path('profile/<int:user>/', ProfileView.as_view(), name='profile-detail')
+    path('profile/<int:user>/', ProfileView.as_view(), name='profile-detail'),
+    path('profiles/business/', ProfileBusinessListView.as_view(), name='profile-business-list'),
+    path('profiles/customer/', ProfileCustomerListView.as_view(), name='profile-customer-list')
 ]
 
--- coderr_app/api/views.py
+++ coderr_app/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
+from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, ListModelMixin
 from coderr_app.models import UserProfile
 from .serializers import UserProfileSerializer, ImageUploadSerializer
 from rest_framework.permissions import AllowAny
@@ -9,14 +9,13 @@ from rest_framework import status
 
 
 class ImageUploadView(APIView):
-    
+
     def post(self, request, format=None):
         serializer = ImageUploadSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
 
 
 class ProfileView(GenericAPIView, RetrieveModelMixin, UpdateModelMixin):
@@ -24,9 +23,26 @@ class ProfileView(GenericAPIView, RetrieveModelMixin, UpdateModelMixin):
     serializer_class = UserProfileSerializer
     permission_classes = [AllowAny]
     lookup_field = 'user'
-            
+
     def get(self, request, *args, **kwargs):
         return super().retrieve(request, *args, **kwargs)
-    
+
     def patch(self, request, *args, **kwargs):
         return super().partial_update(request, *args, **kwargs)
+
+
+class ProfileBusinessListView(GenericAPIView, ListModelMixin):
+    queryset = UserProfile.objects.filter(type='business')
+    serializer_class = UserProfileSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+class ProfileCustomerListView(GenericAPIView, ListModelMixin):
+    queryset = UserProfile.objects.filter(type='customer')
+    serializer_class = UserProfileSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
