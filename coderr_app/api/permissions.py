3062c3359a54a9ea903ebb24b74f8f5839ad5361
johannesbraun54 <johannesbraun02@gmail.com>
2025-05-02 09:23:16 +0200

    updated offer permissions

--- coderr_app/api/permissions.py
+++ coderr_app/api/permissions.py
@@ -19,8 +19,10 @@ class IsOwnerPermission(permissions.BasePermission):
 class IsBusinessUserPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return request.user.is_authenticated
+        if request.method in permissions.SAFE_METHODS and view.action == 'list':
+            return True
+        elif request.method in permissions.SAFE_METHODS and view.action == 'retrieve':
+           return request.user.is_authenticated
         elif request.method in ['POST','PUT', 'PATCH', 'DELETE']:
             authenticated_business_user = bool(request.user.is_authenticated and request.user.userprofile.type == "business")
             return authenticated_business_user
